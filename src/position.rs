a2195913c0401c89297bf976f258192cc010459a
antoninhrlt <antoninhrlt1@gmail.com>
2022-07-14 20:43:01 +0200

    Position to index value and using u32 instead i8

--- src/position.rs
+++ src/position.rs
@@ -11,19 +11,21 @@ use std::char;
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Position {
     x: char,
-    y: i8
+    y: u32
 }
 
 impl Position {
-    pub fn new(x: char, y: i8) -> Self {
+    pub fn new(x: char, y: u32) -> Self {
         Self {
             x,
             y
         }
     }
 
-    pub fn from_i32(i: i32) -> Option<Self> {
-        let x_as_i32: i32 = i - 8 * (i / 8) + 97;
+    /// Obtains a structured position from a dumb i32 index (used to walk 
+    /// through the board's squares)
+    pub fn from_i32(i: u32) -> Option<Self> {
+        let x_as_i32: u32 = i - 8 * (i / 8) + 97;
 
         if x_as_i32 < 0 {
             return None;
@@ -31,10 +33,16 @@ impl Position {
 
         Some(
             Self {
-                x: char::from_u32(x_as_i32 as u32).unwrap(),
-                y: i as i8 / 8 + 1
+                x: char::from_u32(x_as_i32).unwrap(),
+                y: i / 8 + 1
             }
         )
+    }
+
+    /// Destructure the position into a dumb index (used to walk through the
+    /// board's squares)
+    pub fn to_index(&self) -> usize {
+        ((self.y * 8 - 1) + (self.x as u32 - 97 + 1)).try_into().unwrap()
     }
 
     pub fn set_x(&mut self, x: char) -> &mut Self {
@@ -42,7 +50,7 @@ impl Position {
         self
     }
 
-    pub fn set_y(&mut self, y: i8) -> &mut Self {
+    pub fn set_y(&mut self, y: u32) -> &mut Self {
         self.y = y;
         self
     }
@@ -51,7 +59,7 @@ impl Position {
         self.x
     }
 
-    pub fn y(&self) -> i8 {
+    pub fn y(&self) -> u32 {
         self.y
     }
 }
@@ -63,9 +71,16 @@ fn position_from_i32() {
     // 2 x x x x x x x x => 16
     // 3 x x => 18
     //
-    let i: i32 = 18; // so it's {'c', 3}
+    let i: u32 = 18; // so it's {'c', 3}
     
     println!("'a' = {}", 'a' as i32);
     println!("y : {}", i / 8 + 1);
-    println!("{:?}", Position::from_i32(i));
+
+    assert_eq!(Position::from_i32(i), Some(Position::new('c', 3)));
+}
+
+#[test]
+fn position_to_i32() {
+    let position = Position::new('c', 3);
+    assert_eq!(position.to_index(), 18);
 }
